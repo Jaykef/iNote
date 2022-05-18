e12e5f3e42ca0a43d704546ac9ca5689847b5a1d
Jaward Sesay <jkef96@icloud.com>
2022-05-18 15:37:51 +0800

    Update settings.py

--- iNote/iNote/settings.py
+++ iNote/iNote/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-u#01$yr=3zh8@l5$pt)@)96nczsw$(g@&3+lwzy)%4)n=!$dc'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
